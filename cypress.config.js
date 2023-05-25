d7c315d34051ea62039b261effca8028d7b1c97b
Angelina Julie do Nascimento <angelina.julie.dona@gmail.com>
2023-05-25 10:13:05 +0200

    Utilisation de plug-ins de download et upload pour la quest 4

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      on("task", { downloadFile })
+    },
+  },
+});
